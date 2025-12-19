dec425decda4c712fc471a0a46fd384f3889fade
C.S. Melis <cees3331x00@gmail.com>
2025-12-19 22:37:18 +0100

    Remove superfluous line

--- src/main.cpp
+++ src/main.cpp
@@ -226,7 +226,6 @@ void Init()
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
-    UNREFERENCED_PARAMETER(hinstDLL);
     UNREFERENCED_PARAMETER(lpReserved);
 
     if (fdwReason == DLL_PROCESS_ATTACH)
